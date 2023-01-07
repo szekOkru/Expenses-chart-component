1705f5d23cac402b3f34bb5bc93343de2f180ce7
Jakub Poddebniak <jakubpoddd@gmail.com>
2023-01-07 10:36:55 +0100

    Fixed small JS issue

--- main.js
+++ main.js
@@ -23,64 +23,71 @@ function CurrentDay() {
 
 let col1 = document.getElementById("mon");
 col1.addEventListener("click",showTooltip1);
-var visible = false;
+var visible1 = false;
 function showTooltip1() {
-    if (visible == false)
-    { document.getElementById("tool1").className = "visible"; visible = true; }
+    if (visible1 == false)
+    { document.getElementById("tool1").className = "visible"; visible1 = true; }
     else 
-    { document.getElementById("tool1").className = "hidden"; visible = false; }
+    { document.getElementById("tool1").className = "hidden"; visibl1e = false; }
 }
 
 let col2 = document.getElementById("tue");
 col2.addEventListener("click",showTooltip2);
+var visible2 = false;
 function showTooltip2() {
-    if (visible == false)
-    { document.getElementById("tool2").className = "visible"; visible = true; }
+
+    if (visible2 == false)
+    { document.getElementById("tool2").className = "visible"; visible2 = true; }
     else 
-    { document.getElementById("tool2").className = "hidden"; visible = false; }
+    { document.getElementById("tool2").className = "hidden"; visible2 = false; }
 }
 
 let col3 = document.getElementById("wed");
 col3.addEventListener("click",showTooltip3);
+var visible3 = false;
 function showTooltip3() {
-    if (visible == false)
-    { document.getElementById("tool3").className = "visible"; visible = true; }
+    if (visible3 == false)
+    { document.getElementById("tool3").className = "visible"; visible3 = true; }
     else 
-    { document.getElementById("tool3").className = "hidden"; visible = false; }
+    { document.getElementById("tool3").className = "hidden"; visible3 = false; }
 }
 
 let col4 = document.getElementById("thu");
 col4.addEventListener("click",showTooltip4);
+var visible4 = false;
 function showTooltip4() {
-    if (visible == false)
-    { document.getElementById("tool4").className = "visible"; visible = true; }
+    if (visible4 == false)
+    { document.getElementById("tool4").className = "visible"; visible4 = true; }
     else 
-    { document.getElementById("tool4").className = "hidden"; visible = false; }
+    { document.getElementById("tool4").className = "hidden"; visible4 = false; }
 }
 
 let col5 = document.getElementById("fri");
 col5.addEventListener("click",showTooltip5);
+var visible5 = false;
 function showTooltip5() {
-    if (visible == false)
-    { document.getElementById("tool5").className = "visible"; visible = true; }
+    if (visible5 == false)
+    { document.getElementById("tool5").className = "visible"; visible5 = true; }
     else 
-    { document.getElementById("tool5").className = "hidden"; visible = false; }
+    { document.getElementById("tool5").className = "hidden"; visible5 = false; }
 }
 
 let col6 = document.getElementById("sat");
 col6.addEventListener("click",showTooltip6);
+var visible6 = false;
 function showTooltip6() {
-    if (visible == false)
-    { document.getElementById("tool6").className = "visible"; visible = true; }
+    if (visible6 == false)
+    { document.getElementById("tool6").className = "visible"; visible6 = true; }
     else 
-    { document.getElementById("tool6").className = "hidden"; visible = false; }
+    { document.getElementById("tool6").className = "hidden"; visible6 = false; }
 }
 
 let col7 = document.getElementById("sun");
 col7.addEventListener("click",showTooltip7);
+var visible7 = false;
 function showTooltip7() {
-    if (visible == false)
-    { document.getElementById("tool7").className = "visible"; visible = true; }
+    if (visible7 == false)
+    { document.getElementById("tool7").className = "visible"; visible7 = true; }
     else 
-    { document.getElementById("tool7").className = "hidden"; visible = false; }
+    { document.getElementById("tool7").className = "hidden"; visible7 = false; }
 }
